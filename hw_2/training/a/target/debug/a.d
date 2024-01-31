3101b72a65944aaaf6a440c25cc66bd0148851c8
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-01-30 23:06:35 -0800

    Remove rust stuff

--- hw_2/training/a/target/debug/a.d
+++
@@ -1,1 +0,0 @@
-/Users/scott/GitHub/CS218/hw_2/training/a/target/debug/a: /Users/scott/GitHub/CS218/hw_2/training/a/src/main.rs
--- hw_2/training/a/target/debug/deps/a-6928e8cf728199fb.d
+++
@@ -1,5 +0,0 @@
-/Users/scott/GitHub/CS218/hw_2/training/a/target/debug/deps/liba-6928e8cf728199fb.rmeta: src/main.rs
-
-/Users/scott/GitHub/CS218/hw_2/training/a/target/debug/deps/a-6928e8cf728199fb.d: src/main.rs
-
-src/main.rs:
