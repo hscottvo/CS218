6c4d55d6e18a89cd550c19f3894d120f54320b91
Scott <hscottvo@gmail.com>
2024-02-16 10:29:50 -0800

    Add motorcade solution

--- hw_3/challenge/a_motorcade/main.cpp
+++ hw_3/challenge/a_motorcade/main.cpp
@@ -14,6 +14,13 @@ void print(vector<long long> vec) {
   cout << endl;
 }
 
+void print(vector<double> vec) {
+  for (auto i : vec) {
+    cout << i << ' ';
+  }
+  cout << endl;
+}
+
 int main() {
   long long W, L, n;
   cin >> W >> L >> n;
@@ -31,30 +38,57 @@ int main() {
     input.push_back(make_pair(weight, speed));
   }
 
-  print(input);
+  // print(input);
+  //
+  // pre_sum_weight.at(0) = input.at(0).first;
+  // for (unsigned int i = 1; i < pre_sum_weight.size(); i++) {
+  //   pre_sum_weight.at(i) = pre_sum_weight.at(i - 1) + input.at(i).first;
+  // }
+  //
+  // print(pre_sum_weight);
+  //
+  // vector<long long> pre_sum_time(num_elements, -1);
+  // pre_sum_time.at(0) = input.at(0).first;
+  // for (unsigned int i = 1; i < pre_sum_time.size(); i++) {
+  //   pre_sum_time.at(i) = pre_sum_time.at(i - 1) + input.at(i).first;
+  // }
+  //
+  // print(pre_sum_time);
+  vector<double> dp(num_elements, -1);
+  vector<long long> weights(num_elements, -1);
+  dp.at(0) = 0;
+  weights.at(0) = W;
 
-  pre_sum_weight.at(0) = input.at(0).first;
-  for (unsigned int i = 1; i < pre_sum_weight.size(); i++) {
-    pre_sum_weight.at(i) = pre_sum_weight.at(i - 1) + input.at(i).first;
-  }
+  for (unsigned int i = 1; i < input.size(); i++) {
+    // cout << "i: " << i << endl;
+    long long amt = W;
 
-  print(pre_sum_weight);
+    // assume run i'th car by itself.
+    long long min_speed = input.at(i).second;
+    dp.at(i) = ((double)L / min_speed) * 60 + dp.at(i - 1);
 
-  vector<long long> pre_sum_time(num_elements, -1);
-  pre_sum_time.at(0) = input.at(0).first;
-  for (unsigned int i = 1; i < pre_sum_time.size(); i++) {
-    pre_sum_time.at(i) = pre_sum_time.at(i - 1) + input.at(i).first;
-  }
-
-  print(pre_sum_time);
-
-  for (unsigned int i = 1; i < pre_sum_weight.size(); i++) {
-    long long sum = input.at(i).first;
-    long long j = i - 1;
+    long long j = i;
     for (; j >= 0; j--) {
-      cout << "isrentreint" << endl;
+      if (j == 0)
+        break;
+
+      if (input.at(j).first > amt) {
+        break;
+      }
+      amt -= input.at(j).first;
+      min_speed = min(input.at(j).second, min_speed);
+      double new_val = ((double)L / min_speed) * 60 + dp.at(j - 1);
+
+      if (new_val < dp.at(i)) {
+        dp.at(i) = new_val;
+        weights.at(i) = amt;
+      }
+      // cout << "\tamt: " << amt << " j: " << j << endl;
     }
+    // cout << "\tFrom " << j + 1 << " to " << i << endl;
   }
+  // print(dp);
+  cout << fixed << showpoint << setprecision(8) << dp.at(dp.size() - 1) << endl;
 
   return 0;
 }
